6068339771639cf11a6f91fd5a058b344251bc21
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-07-22 22:25:17 +0000

    [add] create apis

---
+++ backend/src/api/community-release/routes/community-release.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * community-release router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::community-release.community-release');
