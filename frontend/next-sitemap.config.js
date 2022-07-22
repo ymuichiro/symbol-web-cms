a0e84c4180a4e0abcaf2f389498b3c70999aa73c
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-07-22 00:19:24 +0000

    [add] add plugins

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://example.com",
+  generateRobotsTxt: true,
+};
+
+export default config;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+  pwa: {
+    dest: "public",
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
