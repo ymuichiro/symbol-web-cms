0b5c4720a8f68ca02c66f5f66400b7e9bdb60fea
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-07-22 00:19:47 +0000

    [add] setup mui-material

---
+++ frontend/pages/_documents.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content={process.env.NEXT_PUBLIC_SITE_DESCRIPTION}
+          />
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#01579b" />
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
