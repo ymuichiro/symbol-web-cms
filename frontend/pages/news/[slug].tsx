c6babd0a5fadd82acd053f0aed812c659cd88f14
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-07-25 10:18:08 +0000

    [add] add router to header & add frame to all page

--- frontend/pages/news/[slug].tsx
+++ frontend/pages/news/[slug].tsx
@@ -3,7 +3,22 @@
   Symbol関連の情報のうち、公的な情報の取り扱いページ（インデックス側より飛んだ先のページ）
 
 */
+import type { NextPage } from 'next';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Header from '../../components/moleculs/Header';
+import Footer from '../../components/moleculs/Footer';
 
-const News = () => {};
+const NewsArticle: NextPage = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.between('xs', 'md'));
 
-export default News;
+  return (
+    <div style={{ marginBottom: '5vh' }}>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default NewsArticle;
