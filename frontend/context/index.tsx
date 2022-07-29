0cee5b31a63a92a5039a95022c628d3356666f90
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-07-29 04:55:01 +0000

    [add] create internationalize

--- frontend/context/index.tsx
+++ frontend/context/index.tsx
@@ -1,18 +1,13 @@
 import React, { useReducer } from 'react';
-import { LangsCode } from '../model/SystemModel';
-import UtilService from '../service/UtilService';
 import { ContextAction, ContextState, ContextStore } from './type';
 
-const initialState: ContextState = {
-  lang: UtilService.getBrowserLang(),
-};
+const initialState: ContextState = {};
 
 export const SystemContext = React.createContext<ContextStore>({
   contextState: initialState,
   updateContext: () => {},
 });
 
-// TODO: https://itnext.io/react-hooks-with-context-as-a-state-management-solution-526d1c13a07d
 const contextReducer = (state: ContextState, action: ContextAction) => {
   switch (action.type) {
     case 'update':
--- frontend/pages/community.tsx
+++ frontend/pages/community.tsx
@@ -8,13 +8,12 @@ import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Header from '../components/moleculs/Header';
 import Footer from '../components/moleculs/Footer';
-import { useContext } from 'react';
-import { SystemContext } from '../context';
+import { useLocale } from '../hooks/useLocale';
 
 const Community: NextPage = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.between('xs', 'md'));
-  const { contextState, updateContext } = useContext(SystemContext);
+  const { t, locale } = useLocale();
 
   return (
     <div style={{ marginBottom: '5vh' }}>
