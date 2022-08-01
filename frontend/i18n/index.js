bce97527779efc37e2161645308022c02edead30
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-08-01 00:50:51 +0000

    [add] yarn run dev で hyderation error に対処

--- frontend/i18n/index.js
+++ frontend/i18n/index.js
@@ -1,3 +1,9 @@
+/*
+
+ 注意：このファイルは node_modules 内部の next-export-i18n から読み込みを行なっている為、
+      ファイル名を変更すると作動しなくなります
+
+*/
 const en = require('./translations.en.json');
 const ja = require('./translations.ja.json');
 
@@ -7,7 +13,7 @@ const i18n = {
     ja: ja.i18n,
   },
   defaultLang: 'en',
-  useBrowserDefault: true,
+  useBrowserDefault: process.env.NODE_ENV === 'production',
 };
 
 module.exports = i18n;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,10 +4,6 @@ let nextConfig = undefined;
 
 if (process.env.NODE_ENV === 'development') {
   nextConfig = {
-    // i18n: {
-    //   locales: ['en', 'ja'],
-    //   defaultLocale: 'en',
-    // },
     reactStrictMode: true,
     swcMinify: true,
     images: {
