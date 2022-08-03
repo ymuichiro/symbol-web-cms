8d0b02ca2aef8027899f81d4f29ca2e9910f232c
toshiya matsukawa <matsukawa5955@gmail.com>
2022-08-03 07:28:02 +0000

    ui

--- backend/src/extensions/users-permissions/strapi-server.js
+++ backend/src/extensions/users-permissions/strapi-server.js
@@ -1,46 +1,46 @@
-const _ = require('lodash');
+const { getService } = require('@strapi/plugin-users-permissions/server/utils');
 
 module.exports = (plugin) => {
     const getController = name => {
         return strapi.plugins['users-permissions'].controller(name);
     };
 
-    // Create the new controller
-    plugin.controllers.auth.register = async (ctx) => {
-        const { email, username, role, token } = ctx.request.body;
-        
+    plugin.controllers.user.create = async (ctx) => {
+        const { email, username, role, token, symbolAddress} = ctx.request.body;
+        console.log(token)
         if(token !== "test") throw new Error('This token is not allowed');
-    
+
         const controller = getController("user");
         const newUser = {
             //...profile,
             //username: "aaa",
-            email: "bbb@gmail.com",
-            provider: "symbol",
-            password: "Toshiya5955!",
+            symbolAddress,
+            //provider: "symbol",
+            //password: "Toshiya5955!",
             //role: 1,
             //confirmed: true,
         };
-        strapi.entityService.create('plugin::users-permissions.user', {
-            data: newUser,
-            populate: ['role'],
-        });
-        /*controller
-          .create({ data: newUser });
-          console.log('calling about meeeeeeeeeee------')*/
+        const data = await getService('user').add(newUser);
+        //strapi.entityService.create('plugin::users-permissions.user', {
+        //    data: newUser,
+        //    populate: ['role'],
+        //});
+          console.log('calling about meeeeeeeeeee------')
 
         return true;
     };
 
     // Add the custom route
+    /*
     plugin.routes['content-api'].routes.unshift({
         method: 'POST',
-        path: '/auth/local/register',
-        handler: 'auth.register',
+        path: '/users',
+        handler: 'user.create',
         config: {
             prefix: '',
         }
     });
+    */
 
     // Create the new controller
     /*
