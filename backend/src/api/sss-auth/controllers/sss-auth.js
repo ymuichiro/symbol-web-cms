6d812329dcb03e6ba91796ad91e61dc3334aaf47
toshiya matsukawa <matsukawa5955@gmail.com>
2022-08-09 09:09:57 +0000

    [add] initial super admin using SSS

---
+++ backend/src/api/sss-auth/controllers/sss-auth.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `sss-auth`
+ */
+
+const symbol = require('symbol-sdk');
+
+module.exports = {
+  async setPassword(ctx, next) {
+    try {
+      const { address, publicKey } = ctx.request.body;
+      const nonce = symbol.Crypto.randomBytes(8);
+      const password = symbol.Convert.uint8ToHex(nonce);
+      const user = await strapi.query('admin::user').findOne({ where: { email: address } });
+      if (user == null) throw new Error('Account does not exist');
+      await strapi.entityService.update('admin::user', user.id, {
+        data: {
+          password,
+          isActive: true,
+          username: address.replace('@mail.com', '').toUpperCase()
+        },
+      });
+      const networkType = process.env.NETWORKTYPE == '152' ? symbol.NetworkType.TEST_NET : symbol.NetworkType.MAIN_NET;
+      const encryptedMessage = symbol.EncryptedMessage.create(password, symbol.PublicAccount.createFromPublicKey(publicKey, networkType), process.env.ADMIN_PRIVATEKEY)
+      const encryptedPayload = encryptedMessage.payload;
+      return [encryptedPayload, process.env.ADMIN_PUBLICKEY]
+    } catch (err) {
+      ctx.badRequest("Account does not exist", { moreDetails: err });
+    }
+  },
+  async getAdminPublicKey(ctx, next) {
+    try {
+      return process.env.ADMIN_PUBLICKEY;
+    } catch (err) {
+      ctx.badRequest("Pubkey does not exist", { moreDetails: err });
+    }
+  },
+};
