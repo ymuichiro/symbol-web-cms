cf501fabf695ac99c0c3d23cbdbb7b54673cad2d
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-09-26 02:29:57 +0000

    [add] add environment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,30 +1,37 @@
 const path = require('path');
 
-// mysql ver
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "mysql",
-//     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 3306),
-//       database: env("DATABASE_NAME", "strapi"),
-//       user: env("DATABASE_USER", "strapi"),
-//       password: env("DATABASE_PASS", "strapi"),
-//       ssl: {
-//         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-//       },
-//     },
-//     debug: false,
-//   },
-// });
-
-// sqlite ver
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+module.exports = ({ env }) => {
+  console.log("-".repeat(5), "connecting database", "-".repeat(5));
+  if (env("STRAPI_NODE_ENV") === "production") {
+    console.log("mode", env("STRAPI_NODE_ENV"), "mysql");
+    // mysql
+    return {
+      connection: {
+        client: "mysql",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 3306),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USER", "strapi"),
+          password: env("DATABASE_PASS", "strapi"),
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+          },
+        },
+        debug: false,
+      },
+    }
+  } else {
+    console.log("mode", env("STRAPI_NODE_ENV"), "sqlite");
+    // sqlite
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
+      },
+    }
+  }
+};
