2eb0c409489296284a18ee26e47ed89491b36311
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-09-26 22:14:57 +0000

    [add] google analytics

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@mui/styles';
+import { GA_TRACKING_ID } from '../service/gtag';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#01579b" />
           <meta
