3d07ef5a8ba1fe51ef365a01608fb6417a81925b
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-11-28 00:04:45 +0900

    [fix] Fixed an issue that prevented Strapi from starting * Unintended behavior occurred because the version of Strapi had been upgraded. * Pin to version 4.2.3

--- backend/src/admin/webpack.config.js
+++ backend/src/admin/webpack.config.js
@@ -1,23 +1,23 @@
-"use strict";
-const path = require("path");
-const strapiCacheSrcPath = path.resolve(__dirname, "../../.cache/admin/src");
+'use strict';
+const path = require('path');
+const strapiCacheSrcPath = path.resolve(__dirname, '../../.cache/admin/src');
 /* eslint-disable no-unused-vars */
 module.exports = (config, webpack) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config
   config.plugins.push(
-    new webpack.NormalModuleReplacementPlugin(
-      /admin\/src\/(.*).js/,
-      (resource) => {
-        if (resource.createData.resource){
-          const replacedFilePath = resource.createData.resource.replace(strapiCacheSrcPath,path.resolve(__dirname,'ui'));
-          if (replacedFilePath.endsWith('.js')) {
-            resource.createData.resource = replacedFilePath;
-          }
+    new webpack.NormalModuleReplacementPlugin(/admin\/src\/(.*).js/, (resource) => {
+      if (resource.createData.resource) {
+        const replacedFilePath = resource.createData.resource.replace(
+          strapiCacheSrcPath,
+          path.resolve(__dirname, 'ui')
+        );
+        if (replacedFilePath.endsWith('.js')) {
+          resource.createData.resource = replacedFilePath;
         }
       }
-    )
+    })
   );
 
   return config;
