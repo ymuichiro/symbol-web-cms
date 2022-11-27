bb3b3b8e361fa949259d2df212fc7aaff1fe2b73
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-11-28 01:37:34 +0900

    [add] update strapi config

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
 async function isFirstRun() {
   const pluginStore = strapi.store({
     environment: strapi.config.environment,
-    type: "type",
-    name: "setup",
+    type: 'type',
+    name: 'setup',
   });
-  const initHasRun = await pluginStore.get({ key: "initHasRun" });
-  await pluginStore.set({ key: "initHasRun", value: true });
+  const initHasRun = await pluginStore.get({ key: 'initHasRun' });
+  await pluginStore.set({ key: 'initHasRun', value: true });
   return !initHasRun;
 }
 
 async function setPublicPermissions(newPermissions) {
-  const publicRole = await strapi.query("plugin::users-permissions.role").findOne({
+  const publicRole = await strapi.query('plugin::users-permissions.role').findOne({
     where: {
-      type: "public",
+      type: 'public',
     },
   });
 
@@ -23,7 +23,7 @@ async function setPublicPermissions(newPermissions) {
   Object.keys(newPermissions).map((controller) => {
     const actions = newPermissions[controller];
     const permissionsToCreate = actions.map((action) => {
-      return strapi.query("plugin::users-permissions.permission").create({
+      return strapi.query('plugin::users-permissions.permission').create({
         data: {
           action: `api::${controller}.${controller}.${action}`,
           role: publicRole.id,
@@ -38,42 +38,42 @@ async function setPublicPermissions(newPermissions) {
 // Create the new localization
 async function setInternationalization(newLocalizations) {
   for (const l of newLocalizations) {
-    await strapi.query("plugin::i18n.locale").create({
+    await strapi.query('plugin::i18n.locale').create({
       data: {
         name: l.name,
         code: l.code,
-      }
-    })
+      },
+    });
   }
-
 }
 
 async function importSeedData() {
   await setPublicPermissions({
-    "community-release": ["find", "findOne"],
-    "news-release": ["find", "findOne"],
-    document: ["find", "findOne"],
+    'community-release': ['find', 'findOne'],
+    'news-release': ['find', 'findOne'],
+    document: ['find', 'findOne'],
+    space: ['find', 'findOne'],
   });
   await setInternationalization([
-    { name: "Japanese (Japan) (ja-JP)", code: "ja-JP" },
-    { name: "Russian (Russia) (ru-RU)", code: "ru-RU" },
-    { name: "Korean (ko)", code: "ko" },
+    { name: 'Japanese (Japan) (ja-JP)', code: 'ja-JP' },
+    { name: 'Russian (Russia) (ru-RU)', code: 'ru-RU' },
+    { name: 'Korean (ko)', code: 'ko' },
   ]);
 }
 
 module.exports = async () => {
   const shouldImportSeedData = await isFirstRun();
-  console.log("-".repeat(5), "Run Strapi bootstrap", "-".repeat(5))
+  console.log('-'.repeat(5), 'Run Strapi bootstrap', '-'.repeat(5));
   if (shouldImportSeedData) {
     try {
-      console.log("Setting up the template...");
+      console.log('Setting up the template...');
       await importSeedData();
-      console.log("Ready to go");
+      console.log('Ready to go');
     } catch (error) {
-      console.log("Could not import seed data");
+      console.log('Could not import seed data');
       console.error(error);
     }
   }
-  console.log("bootstrap configured")
-  console.log("-".repeat(5), "end", "-".repeat(5))
+  console.log('bootstrap configured');
+  console.log('-'.repeat(5), 'end', '-'.repeat(5));
 };
